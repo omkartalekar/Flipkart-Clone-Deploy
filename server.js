a431687928b5e2944cc582bb59b65423e40d5d8d
omkartalekar <omkartalekar1112@gmail.com>
2023-10-10 00:04:02 +0530

    new commit

--- server.js
+++ server.js
@@ -5,46 +5,48 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import path from "path";
 import { fileURLToPath } from "url";
-// (node:15846) DeprecationWarning: collection.ensureIndex is deprecated. Use createIndexes instead. (Use `node --trace-deprecation ...` to show where the warning was created)
-//Mongoose is now at v5.4.13. Per their docs, these are the fixes for the deprecation warnings...
-mongoose.set("useNewUrlParser", true);
-mongoose.set("useFindAndModify", false);
-mongoose.set("useCreateIndex", true);
-
 import Connection from "./database/db.js";
 import Routes from "./routes/route.js";
 import DefaultData from "./default.js";
 
 const __filename = fileURLToPath(import.meta.url);
-
 const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
 const app = express();
-
-const PORT = 8000;
-
+const PORT = process.env.PORT || 8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
-Connection(username, password);
-DefaultData();
+// Set mongoose options to avoid deprecation warnings
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 
+// Connect to the database
+Connection(username, password);
+
+DefaultData();
+// Serve static files
 app.use(express.static(path.join(__dirname, "./client/build")));
 
-Connection(username, password);
+// Parse request bodies
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// Enable CORS
+app.use(cors());
 
+// Define API routes
+app.use("/", Routes);
+
+// Handle all other routes by serving the React app
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+// Start the server
 app.listen(PORT, () =>
   console.log(`Server is running successfully on PORT ${PORT}`)
 );
-
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.use("/", Routes);
