66d63ca971df7ae5ada02860777b0f262e6e26e1
omkartalekar <omkartalekar1112@gmail.com>
2023-10-10 21:57:38 +0530

    new

--- database/db.js
+++ database/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const Connection = async (username, password) => {
-        const URL = `mongodb+srv://${username}:${password}@flipkartclone.p5wcl1i.mongodb.net/?retryWrites=true&w=majority`;
+        const URL =`mongodb+srv://${username}:${password}@flipkartclone.p5wcl1i.mongodb.net/?retryWrites=true&w=majority`
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
         console.log('Database Connected Succesfully');
